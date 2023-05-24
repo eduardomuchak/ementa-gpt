e5afbfb19085a04ed9b32f6df832c7936f524ac7
Eduardo Muchak <eduardomuchak@gmail.com>
2023-05-24 10:49:17 -0300

    feat: :sparkles: adiciona requisicao para api gpt

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -2,19 +2,23 @@ import { openai } from "@/lib/openai";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  console.log("request =>", request);
-
   const { prompt } = await request.json();
 
   try {
-    const completion = await openai.createCompletion({
+    const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      prompt: `Você é uma ferramenta de criação de ementas de acórdãos de Tribunais. Seu papel é ler o acórdão e criar uma ementa para ele. \n\nAcórdão: ${prompt}`,
+      messages: [
+        {
+          role: "system",
+          content:
+            "Você é uma ferramenta de criação de ementas de acórdãos de Tribunais. Seu papel é ler o acórdão e criar uma ementa para ele.",
+        },
+        { role: "user", content: prompt },
+      ],
     });
-
-    console.log("DEU CERTO =>", completion);
-
-    return NextResponse.json({ text: `${completion.data.choices[0].text}` });
+    return NextResponse.json({
+      result: completion.data.choices[0].message?.content,
+    });
   } catch (err) {
     console.log("ERRO =>", err);
   }
--- src/components/InitialForm/index.tsx
+++ src/components/InitialForm/index.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { z } from "zod";
 import { FileDropzone } from "../FileDropzone";
 
@@ -17,7 +18,7 @@ export function InitialForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    formState: { errors },
   } = useForm<FormSchema>({
     defaultValues: {
       peticaoInicial: "",
@@ -25,60 +26,71 @@ export function InitialForm() {
     resolver: zodResolver(formSchema),
   });
 
+  const [result, setResult] = useState<null | string>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   async function onSubmit(data: FormSchema) {
+    setIsLoading(true);
     const response = await axios.post("api/openai", {
       prompt: data.peticaoInicial,
-      Headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-      },
     });
-
-    console.log("RESPOSTA =>", response);
+    setResult(JSON.stringify(response.data.result));
   }
 
+  useEffect(() => {
+    if (result) {
+      setIsLoading(false);
+    }
+  }, [result]);
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="space-y-6">
-        <div className="flex flex-col">
-          <label
-            className="font-ubuntu text-base font-bold uppercase"
-            htmlFor={"peticaoInicial"}
-          >
-            Digite o nome da ação conforme petição inicial:
-            <input
-              className="relative z-auto mt-1 flex h-14 w-full appearance-none flex-row justify-between rounded-xl border-green-primary-dark bg-input px-7 py-4 font-ubuntu text-gray-800 transition-all duration-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-green-primary-dark"
-              type="text"
-              id="peticaoInicial"
-              {...register("peticaoInicial")}
-            />
-          </label>
-          {errors.peticaoInicial?.message ? (
-            <span className="mt-1 font-ubuntu text-xs font-medium text-red-500">
-              {errors.peticaoInicial?.message}
-            </span>
-          ) : null}
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-6">
+          <div className="flex flex-col">
+            <label
+              className="font-ubuntu text-base font-bold uppercase"
+              htmlFor={"peticaoInicial"}
+            >
+              ACÓRDÃO:
+              <textarea
+                className="mt-1 flex w-full flex-row justify-between rounded-lg border-green-primary-dark bg-input px-7 py-4 font-ubuntu text-gray-800 transition-all duration-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-green-primary-dark"
+                id="peticaoInicial"
+                {...register("peticaoInicial")}
+              />
+            </label>
+            {errors.peticaoInicial?.message ? (
+              <span className="mt-1 font-ubuntu text-xs font-medium text-red-500">
+                {errors.peticaoInicial?.message}
+              </span>
+            ) : null}
+          </div>
+
+          <div className="flex h-80 w-full items-center justify-center rounded-xl bg-green-primary-light p-8">
+            {isLoading ? (
+              <div className="flex flex-1 flex-col items-center justify-center gap-4">
+                <span className="font-ubuntu text-2xl font-bold">
+                  Gerando ementa...
+                </span>
+              </div>
+            ) : (
+              <FileDropzone />
+            )}
+          </div>
         </div>
 
-        <div className="flex h-80 w-full items-center justify-center rounded-xl bg-green-primary-light p-8">
-          <FileDropzone />
-        </div>
-      </div>
-
-      {isSubmitting && (
+        <button
+          type={"submit"}
+          className="rounded-xl bg-green-500 px-3 py-2 text-base font-bold text-white"
+        >
+          Confirmar
+        </button>
+      </form>
+      {result ? (
         <div className="flex flex-col items-center justify-center gap-4">
-          <span className="font-ubuntu text-base font-bold ">
-            Gerando ementa...
-          </span>
+          <span className="font-ubuntu text-base font-bold ">{result}</span>
         </div>
-      )}
-
-      <button
-        type={"submit"}
-        className="rounded-xl bg-green-500 px-3 py-2 text-base font-bold text-white"
-      >
-        Confirmar
-      </button>
-    </form>
+      ) : null}
+    </>
   );
 }
